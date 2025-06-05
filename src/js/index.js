b34fa6655bc7deb86b8085f0ae833b7efe111db3
Flippont <144795506+flippont@users.noreply.github.com>
2025-06-05 22:34:02 +0100

    caching is now gone

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,4 @@
 let currentPage = 'home';
-// Store loaded pages to avoid refetching
-const pages = {};
 
 function getPageFromUrl() {
     const params = new URLSearchParams(window.location.search);
@@ -56,25 +54,18 @@ function loadPage(page, push = true) {
     if (push) {
         setPageInUrl(page, true);
     }
-
-    // If already loaded, use cache
-    if (pages[page]) {
-        document.querySelector('.container').innerHTML = pages[page];
-        executeScripts(document.querySelector('.container'));
-    } else {
-        document.querySelector('.container').innerHTML = '<div class="loading">Loading...</div>';
-        fetch(`https://flippont.github.io/src/pages/${page}.html`)
-            .then(response => response.text())
-            .then(data => {
-                pages[page] = data;
-                document.querySelector('.container').innerHTML = data;
-                executeScripts(document.querySelector('.container'));
-            })
-            .catch(error => {
-                document.querySelector('.container').innerHTML = '<div class="error">Error loading page.</div>';
-                console.error('Error loading page:', error);
-            });
-    }
+    document.querySelector('.container').innerHTML = '<div class="loading">Loading...</div>';
+    fetch(`https://flippont.github.io/src/pages/${page}.html`)
+        .then(response => response.text())
+        .then(data => {
+            pages[page] = data;
+            document.querySelector('.container').innerHTML = data;
+            executeScripts(document.querySelector('.container'));
+        })
+        .catch(error => {
+            document.querySelector('.container').innerHTML = '<div class="error">Error loading page.</div>';
+            console.error('Error loading page:', error);
+        });
     currentPage = page;
 }
 
