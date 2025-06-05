34d22ab5f5d54edea603b14cd2060d4e3e66f3f6
Flippont <144795506+flippont@users.noreply.github.com>
2025-06-05 21:52:01 +0100

    Script update

--- src/js/index.js
+++ src/js/index.js
@@ -21,32 +21,12 @@ function executeScripts(container) {
     const scripts = container.querySelectorAll('script');
     scripts.forEach(oldScript => {
         const src = oldScript.getAttribute('src');
-        if (src) {
-            // Check if script with this src is already loaded
-            if (!document.querySelector(`script[src="${src}"]`)) {
-                const newScript = document.createElement('script');
-                newScript.src = src;
-                // Copy other attributes if needed
-                for (const attr of oldScript.attributes) {
-                    if (attr.name !== 'src') {
-                        newScript.setAttribute(attr.name, attr.value);
-                    }
-                }
-                container.appendChild(newScript);
-            }
-            oldScript.parentNode.removeChild(oldScript);
-        } else {
-            // Inline script: always execute
+        if (!document.querySelector(`script[src="${src}"]`)) {
             const newScript = document.createElement('script');
-            for (const attr of oldScript.attributes) {
-                newScript.setAttribute(attr.name, attr.value);
-            }
-            if (oldScript.textContent) {
-                newScript.textContent = oldScript.textContent;
-            }
-            oldScript.parentNode.removeChild(oldScript);
+            newScript.src = src;
             container.appendChild(newScript);
         }
+        oldScript.parentNode.removeChild(oldScript);
     });
 }
 
