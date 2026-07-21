0b98a23ac4916f183d71b352af761d9f51db1139
Flippont <144795506+flippont@users.noreply.github.com>
2026-07-21 19:19:43 +0100

    cursor change

--- js/index.js
+++ js/index.js
@@ -12,8 +12,8 @@ function moveIntoView(element) {
 (function() {
     document.title = `Flippont / ${currentTab} / ${currentPage}`;
     document.querySelector("header").innerHTML = `
-        ${currentPage == "index" ? "" : currentPage}<br>
-        <a href="${currentTab == "home" ? "./" : "../"}index.html" target=_self>${currentPage == "index" || currentTab == "blog" && currentPage != "blog" ? "" : "<-- Home"}</a>
+        ${currentTab == "home" ? "" : currentPage}<br>
+        <a href="${currentTab == "home" ? "./" : "../"}index.html" target=_self>${currentTab == "home" || currentTab == "blog" && currentPage != "blog" ? "" : "<-- Home"}</a>
     `;
     if(currentTab != "home" && currentPage != "" && currentPage != "blog"){    
         s = document.createElement("SCRIPT");
